71258bd41079c0a22eb819ae24b79440848991be
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-28 13:56:41 -0700

    Added Db Context Class

---
+++ CPW - 219 eCommerceSite/Data/ItemContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW___219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW___219_eCommerceSite.Data
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<item> Items { get; set; }
+    }
+}

27ec3fbbe7f4d5bbbd93c2bc828f0240dcb48a45
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-28 13:11:11 -0700

    Added Item Class

---
+++ CPW - 219 eCommerceSite/Models/item.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW___219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single item for sale
+    /// </summary>
+    public class item
+    {
+        /// <summary>
+        /// The unique identifier for the item
+        /// </summary>
+        [Key]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// The name of the item
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The sale price of the item
+        /// </summary>
+        [Range(0, 1000)]
+        public double Price { get; set; }
+
+        // Todo: add country of origin
+    }
+}

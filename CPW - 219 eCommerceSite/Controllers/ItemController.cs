32e026542d0fb9612ac788fde57e45d312cc147a
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-04 21:26:40 -0700

    Create item fully functional, added some styling

--- CPW - 219 eCommerceSite/Controllers/ItemController.cs
+++ CPW - 219 eCommerceSite/Controllers/ItemController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW___219_eCommerceSite.Data;
+using CPW___219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW___219_eCommerceSite.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ItemContext _context;
+
+        public ItemController(ItemContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(item newItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(newItem); // prepares insert
+                _context.SaveChanges(); // executes pending insert
+                ViewData["Message"] = $"{newItem.Name} was added successfully!";
+                // Show success message on page
+                return View();
+            }
+
+            return View(newItem);
         }
     }
 }

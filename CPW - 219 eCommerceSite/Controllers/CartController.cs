cd8b6d32d25f8d08c5d478950665762cbc5c36e2
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-18 15:37:11 -0700

    Add shopping cart button to Item Catalog

---
+++ CPW - 219 eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CPW___219_eCommerceSite.Data;
+using CPW___219_eCommerceSite.Models;
+
+namespace CPW___219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ItemContext _context;
+
+        public CartController(ItemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            item? itemToAdd = _context.Items.Where(i => i.ItemId == id).SingleOrDefault();
+
+            if (itemToAdd == null)
+            {
+                // item with that id not found
+                TempData["Message"] = "Sorry that item was not found";
+                return RedirectToAction("Index", "Item");
+            }
+
+            // Todo: Add item to a shopping cart cookie
+            TempData["Message"] = "Item added to cart";
+            return RedirectToAction("Index", "Item");
+        }
+    }
+}

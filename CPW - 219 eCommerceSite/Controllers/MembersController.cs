5671a4cb9ddba4e77f1c8657a21a7657f6a6254c
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-18 10:14:09 -0700

    Added HttpPost for Register

--- CPW - 219 eCommerceSite/Controllers/MembersController.cs
+++ CPW - 219 eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CPW___219_eCommerceSite.Models;
+using CPW___219_eCommerceSite.Data;
 
 namespace CPW___219_eCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly ItemContext _itemContext;
+
+        public MembersController(ItemContext itemContext)
+        {
+            _itemContext = itemContext;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map RegiserViewModel data to Member object
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password
+                };
+
+                _itemContext.Members.Add(newMember);
+                await _itemContext.SaveChangesAsync();
+
+                // Redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(regModel);
         }
     }
 }

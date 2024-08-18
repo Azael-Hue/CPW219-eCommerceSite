881039dd3abc89eb9d73be42e35ba77a9a4e0994
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-18 10:13:47 -0700

    Made Member Phone and Username optional

--- CPW - 219 eCommerceSite/Models/Member.cs
+++ CPW - 219 eCommerceSite/Models/Member.cs
@@ -7,13 +7,13 @@ namespace CPW___219_eCommerceSite.Models
         [Key]
         public int MemberId { get; set; }
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
     }
 
     public class RegisterViewModel

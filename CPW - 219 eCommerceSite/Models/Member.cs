1dc2f002436c659711e2635d86347d1b9c3fd564
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-18 08:09:57 -0700

    Add memeber class

---
+++ CPW - 219 eCommerceSite/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW___219_eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        private int MemberId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string UserName { get; set; }
+    }
+}

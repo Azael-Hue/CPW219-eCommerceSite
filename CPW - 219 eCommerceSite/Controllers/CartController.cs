e6723cd9f3c185833a219e3b78e38b02b73192a1
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-18 17:09:55 -0700

    Coded Add to cart, remove from cart and added a cart summary page

--- CPW - 219 eCommerceSite/Controllers/CartController.cs
+++ CPW - 219 eCommerceSite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CPW___219_eCommerceSite.Data;
 using CPW___219_eCommerceSite.Models;
+using Newtonsoft.Json;
 
 namespace CPW___219_eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
         private readonly ItemContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(ItemContext context)
         {
@@ -24,9 +26,66 @@ namespace CPW___219_eCommerceSite.Controllers
                 return RedirectToAction("Index", "Item");
             }
 
-            // Todo: Add item to a shopping cart cookie
+            CartItemViewModel cartItem = new()
+            {
+                ItemId = itemToAdd.ItemId,
+                Name = itemToAdd.Name,
+                Price = itemToAdd.Price
+            };
+
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+            cartItems.Add(cartItem);
+            WriteShoppingCartCookies(cartItems);
+
             TempData["Message"] = "Item added to cart";
             return RedirectToAction("Index", "Item");
+        }
+
+        private void WriteShoppingCartCookies(List<CartItemViewModel> cartItems)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartItems);
+
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddDays(1)
+            });
+        }
+
+        /// <summary>
+        /// Return the current list of Items in the user's shopping
+        /// cart cookie. If there is no cookie, an empty list will be returned.
+        /// </summary>
+        /// <returns></returns>
+        private List<CartItemViewModel> GetExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrEmpty(cookie))
+            {
+                return new List<CartItemViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartItemViewModel>>(cookie);
+        }
+
+        public IActionResult Summary()
+        {
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+            return View(cartItems);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+
+            CartItemViewModel? targetItem =
+                cartItems.Where(i => i.ItemId == id).FirstOrDefault();
+
+            cartItems.Remove(targetItem);
+
+            WriteShoppingCartCookies(cartItems);
+
+            return RedirectToAction(nameof(Summary));
         }
     }
 }
